3ca967588d8b959cf445d6830db0dafb78b2b87f
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-15 10:26:42 -0300

    DTO com Java Record

---
+++ src/main/java/med/voll/api/controller/DadosCadastroMedico.java
@@ -0,0 +1,14 @@
+package med.voll.api.controller;
+
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.medico.Especialidade;
+
+public record DadosCadastroMedico(
+    String nome, 
+    String email,
+    String telefone, 
+    String crm, 
+    Especialidade especialidade, 
+    DadosEndereco endereco) {
+
+}
