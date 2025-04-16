9e11c60e60f38898c6e73af09512ed16b44e146b
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-16 16:45:23 -0300

    Ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +32,7 @@ public class MedicoController {
     } 
 
     @GetMapping
-    public Page<DadosListagemMedico> listarMedicos(Pageable paginacao) {
+    public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         System.out.println("Listando médicos");
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
