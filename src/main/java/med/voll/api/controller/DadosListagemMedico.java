5ac6d57f068dc0e75627b62e9cd8c31cdd5f8215
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-16 16:55:44 -0300

    Requisições PUT

--- src/main/java/med/voll/api/controller/DadosListagemMedico.java
+++ src/main/java/med/voll/api/controller/DadosListagemMedico.java
@@ -4,12 +4,18 @@ import med.voll.api.medico.Especialidade;
 import med.voll.api.medico.Medico;
 
 public record DadosListagemMedico(
+        Long id,
         String nome,
         String crm,
         String email,
         Especialidade especialidade) {
             
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(
+            medico.getId(), 
+            medico.getNome(), 
+            medico.getEmail(), 
+            medico.getCrm(), 
+            medico.getEspecialidade());
     }
 }
