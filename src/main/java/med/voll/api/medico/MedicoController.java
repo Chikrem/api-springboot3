32891cfbfe36106b580ff29827b4d37412933221
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-22 09:54:13 -0300

    Detalhando dados na API

--- src/main/java/med/voll/api/medico/MedicoController.java
+++ src/main/java/med/voll/api/medico/MedicoController.java
@@ -70,4 +70,10 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalharMedico(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 }
