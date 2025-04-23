a11bc739702f97d27e0848cf80fa9f244c77cef7
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-04-23 17:26:54 -0300

    Validações de integridade

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.infra.exception.ValidacaoException;
 
 @Service
 public class AgendaDeConsultas {
@@ -20,7 +21,9 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados) {
         System.out.println(dados);
-
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var medico = medicoRepository.findById(dados.idMedico()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
